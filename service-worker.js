9cfc0d5716bd77ce465cbd1fef1c33e42ad81c0a
hank102589 <84878710+hank102589@users.noreply.github.com>
2023-08-27 14:33:09 +0800

    V3

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('nuts-pwa-cache-v1').then(cache => {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/main.html', // 根據您的需要添加更多頁面
-          '/assets/css/main.css',
-          '/assets/js/main.js', // 根據您的腳本名稱
-          '/images/android/playstore-icon.png'
+          './',
+          './assets/css/main.css',
+          './assets/js/main.js', // 根據您的腳本名稱
+          './images/android/playstore-icon.png'
         ]);
       })
     );
