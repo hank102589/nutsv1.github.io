7bf8f53ed1d041fcca266992123c10b3e70ed0d9
hank102589 <84878710+hank102589@users.noreply.github.com>
2023-08-27 15:24:37 +0800

    V4

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,28 @@
-// 安裝 Service Worker
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('nuts-pwa-cache-v1').then(cache => {
-        return cache.addAll([
-          './',
-          './assets/css/main.css',
-          './assets/js/main.js', // 根據您的腳本名稱
-          './images/android/playstore-icon.png'
-        ]);
-      })
-    );
-  });
-  
-  // 捕獲請求並返回快取的資源
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('nuts-pwa-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './assets/css/main.css',
+        './assets/js/main.js', // 根據您的腳本名稱
+        './images/android/playstore-icon.png'
+      ]);
+    }).then(() => {
+      self.skipWaiting(); // 強制使新的 Service Worker 取得控制權
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim() // 確保新版本的 Service Worker 立即生效
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
